3756542acdd5bd8dc51538bc7f7eb91aa92c233f
Garuka Veenath Assalaarachchi <119990432+GaruVA@users.noreply.github.com>
2023-08-10 07:53:50 +0530

    Update 2-3.c

--- Lab02/2-3.c
+++ Lab02/2-3.c
@@ -13,7 +13,7 @@ int main() {
 
     printf("Average Speed = %d \n",averageSpeed);
 
-    When division is done with two integers, integer division occurs, which will cut of any decimal values from the answer if there are any.Which will give us a slight error in the answer.
+    When division is done with two integers, integer division occurs, which will cut off any decimal values from the answer if there are any.Which will give us a slight error in the answer.
     In order to stop integer division from happening, both if not one must be a float value, we can use Explicit type conversion in front of the operation.
     */
 
